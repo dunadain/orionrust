ab2d7cd20056811e737c0839dab9431ec63d9c0a
Ryan Sampras <maelendil@gmail.com>
2024-07-27 18:20:55 +0800

    Create app.rs

---
+++ orion/src/app.rs
@@ -0,0 +1,49 @@
+use std::{env, sync::OnceLock};
+
+use tokio::{
+    select,
+    signal::{
+        self,
+        unix::{signal, SignalKind},
+    },
+};
+
+pub struct Application {
+    uuid: u32,
+}
+
+impl Application {
+    pub fn new() -> Self {
+        Application {
+            uuid: env::var("server_id")
+                .unwrap_or_else(|_| 0.to_string())
+                .parse()
+                .expect("server_id should be a number"),
+        }
+    }
+
+    pub fn uuid(&self) -> u32 {
+        self.uuid
+    }
+
+    pub async fn start(&self) {
+        let mut sigterm = signal(SignalKind::terminate()).unwrap();
+        select! {
+            _ = signal::ctrl_c() => {
+                println!("Received SIGINT");
+                self.shutdown();
+            }
+            _ = sigterm.recv() => {
+                println!("Received SIGTERM");
+                self.shutdown();
+            }
+        }
+    }
+
+    fn shutdown(&self) {}
+}
+
+pub fn app() -> &'static Application {
+    static CLIENTMGR: OnceLock<Application> = OnceLock::new();
+    CLIENTMGR.get_or_init(|| Application::new())
+}
