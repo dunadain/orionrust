4628515b1c55fba9677ee5e155d0e619f513bafb
Ryan Sampras <maelendil@gmail.com>
2024-08-08 19:05:01 +0800

    use redis connection manager

--- orion/src/async_redis/mod.rs
+++ orion/src/async_redis/mod.rs
@@ -1,8 +1,11 @@
-use redis::{aio::MultiplexedConnection, Client};
+use redis::{aio::ConnectionManager, Client};
 
-pub async fn connect(url: &str) -> MultiplexedConnection {
+/// connection manager will reconnect to redis if the connection is lost
+pub async fn connect(url: &str) -> ConnectionManager {
     let client = Client::open(url).expect("Failed to create redis client");
-    let con_result = client.get_multiplexed_async_connection().await;
+    // let config = ConnectionManagerConfig::default();
+    // use default config
+    let con_result = client.get_connection_manager().await;
     match con_result {
         Ok(con) => con,
         Err(e) => panic!("Failed to connect to redis: {}", e),
--- orion/src/lib.rs
+++ orion/src/lib.rs
@@ -2,6 +2,7 @@ mod app;
 pub use app::app;
 
 mod net;
+pub use net::nats_client;
 pub use net::tcp::serve_tcp;
 pub use net::tcp::tcp_actors::SocketHandle;
 pub use net::tcp::SocketListener;
