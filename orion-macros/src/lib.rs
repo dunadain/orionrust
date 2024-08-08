b03b1e6733f972bee82d8286426bef65be6a385a
Ryan Sampras <maelendil@gmail.com>
2024-08-08 15:59:05 +0800

    add macro crate

---
+++ orion-macros/src/lib.rs
@@ -0,0 +1,33 @@
+use proc_macro::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, Stmt};
+
+#[proc_macro_attribute]
+pub fn init_tracing(_: TokenStream, item: TokenStream) -> TokenStream {
+    let mut item_fn = parse_macro_input!(item as syn::ItemFn);
+    // eprintln!("itemfn: {:#?}", item_fn);
+    let stmts = &mut item_fn.block.stmts;
+    let stmt1: Stmt = syn::parse2(
+        quote! {
+            let collector = tracing_subscriber::fmt()
+            // filter spans/events with level TRACE or higher.
+            .with_max_level(tracing::Level::INFO)
+            .with_file(true)
+            .with_line_number(true)
+            .with_target(true)
+            .with_span_events(tracing_subscriber::fmt::format::FmtSpan::ENTER | tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+            // build but do not install the subscriber.
+            .finish();
+        }
+    )
+    .unwrap();
+    let stmt2: Stmt = syn::parse2(
+        quote! {
+            tracing::subscriber::set_global_default(collector).expect("setting default subscriber failed");
+        }
+    )
+    .unwrap();
+    stmts.insert(0, stmt2);
+    stmts.insert(0, stmt1);
+    item_fn.to_token_stream().into()
+}
---
+++ orion-macros/src/main.rs
@@ -0,0 +1,9 @@
+use orion_macros::init_tracing;
+use tracing::{debug, error, info};
+
+#[init_tracing]
+fn main() {
+    info!("Hello, world!");
+    debug!("Hello, you!");
+    error!("Hello, error!");
+}
